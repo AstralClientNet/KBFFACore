e13c6559404df66540d109d7847cba3503588af2
BariPlayzYT <rifatbari28@gmail.com>
2021-03-16 20:19:46 +0600

    Initial structure

---
+++ src/TheBarii/KnockbackFFA/Main.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheBarii\KnockbackFFA;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\entity\Entity;
+use pocketmine\item\ItemFactory;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\math\Vector3;
+use pocketmine\utils\TextFormat;
+use pocketmine\nbt\tag\ByteArrayTag;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\network\mcpe\protocol\EventPacket;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+
+class Main extends PluginBase{
+
+
+    
+
+}
