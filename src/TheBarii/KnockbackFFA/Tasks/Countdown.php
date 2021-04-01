6eb9e98f065710441a232217ad2e7c44dc0df717
BariPlayzYT <rifatbari28@gmail.com>
2021-04-01 19:19:16 +0600

    Countdown Thingy

--- src/TheBarii/KnockbackFFA/Tasks/Countdown.php
+++ src/TheBarii/KnockbackFFA/Tasks/Countdown.php
@@ -4,58 +4,24 @@ declare(strict_types=1);
 
 namespace TheBarii\KnockbackFFA\Tasks;
 
-use pocketmine\event\Listener;
 use pocketmine\Player;
-use pocketmine\Server;
-
-use pocketmine\item\Item;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\math\Vector3;
-use pocketmine\network\mcpe\protocol\LevelEventPacket;
-use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
-use pocketmine\network\mcpe\protocol\PlaySoundPacket;
-use pocketmine\event\player\PlayerCreationEvent;
-use pocketmine\inventory\ArmorInventory;
-use pocketmine\inventory\ArmorInventoryEventProcessor;
-use pocketmine\item\Armor;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerExhaustEvent;
-use pocketmine\event\player\PlayerPreLoginEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\item\enchantment\EnchantmentInstance;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
-use pocketmine\event\inventory\CraftItemEvent;
-use pocketmine\event\player\PlayerDropItemEvent;
-use pocketmine\event\entity\EntityDeathEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityRegainHealthEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\entity\EntityLevelChangeEvent;
-use TheBarii\KnockbackFFA\Main;
-use TheBarii\KnockbackFFA\CPlayer;
-use pocketmine\item\enchantment\Enchantment;
-use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\scheduler\Task;
-use pocketmine\block\Sandstone;
-use pocketmine\block\Block;
-use pocketmine\block\Air;
 
 class Countdown extends Task{
 
-    public function __construct(Main $plugin){
+    public $num;
+    public $player;
 
-        $this->plugin = $plugin;
+    public function __construct($num, Player $player){
+
+        $this->num = $num;
+        $this->player = $player;
 
     }
 
     public function onRun(int $tick){
-
-
-
+            $this->player->setTitle("");
+            $this->player->addTitle("§l§cYOU DIED!", "§aRespawning In §b$this->num §aseconds.", 20, 60, 40);
     }
 
 }
